7ec59c23ff3a07aabe299b520daf435211b6374b
hanmowang <hanmow2003@gmail.com>
2022-09-10 17:28:33 -0400

    adding variable

--- delete_tweets.py
+++ delete_tweets.py
@@ -7,8 +7,8 @@ api = tweepy.API(auth)
 
 #the number of tweets and retweets you want to delete
 amt = 300
-
-tweets = api.user_timeline(screen_name = 'replace this with your twitter handle', count = amt, include_rts = True, 
+twitter_handle = 'enter your twitter handle'
+tweets = api.user_timeline(screen_name = twitter_handle, count = amt, include_rts = True, 
                             tweet_mode = 'extended')
 for tweet in tweets:
     api.destroy_status(tweet.id)
